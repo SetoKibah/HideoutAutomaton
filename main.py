cf98c4cf420851ca30f448c72ce62c4ecb40aaef
SetoKibah <setokibah@gmail.com>
2022-06-21 10:53:16 -0600

    Added more items, put print statements to populate terminal in addition to debug logs.

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ logging.basicConfig(level=logging.DEBUG, filename="Automaton.log", filemode="w",
 # Setting our test items list, will select important items later for testing
 items_list = ['pile of meds', 'shampoo', 'slickers', 'wires', 'sj6', '9x19mm rip', 'eagle', 'can of max energy', 
             'scav backpack', 'canister with purified water', 'bottle of water', 'cms surgical kit', 'Can of Hot Rod energy drink',
-             'ox bleach', 'secure flash drive', 'aramid fiber fabric']
+             'ox bleach', 'secure flash drive', 'aramid fiber fabric', 'vodka', 'wilston cigarettes', 'bottle of water', 'emergency water ration',
+             'aquamari water bottle', 'aseptic bandage', 'toilet paper', 'm.u.l.e.', 'propital', 'salewa', 'ifak', 'capacitors', 'printed circuit board',
+             '5.45x39mm pp', '9x18mm PM Rg028', 'weapon parts']
 
 # Function to send query out
 def run_query(query):
@@ -64,8 +66,9 @@ if __name__ == "__main__":
     # Testing list items comprehension with more pertinent information to take
     items_dictionary = {}
     fee_list = []
+    print('Querying our items')
     for item_top in items_list:
-        
+        print(f"{item_top} query starts...")
         logging.info(f"Testing for item query of {item_top}")
         trimmed_result = query_send_receive(item_top)
 
@@ -86,13 +89,9 @@ if __name__ == "__main__":
                 highest_source = item['source']
                 highest_price = item['price']
                 items_dictionary[item_top] = item['price']
+        print(f"{item_top} query complete.")
         logging.info(f"{item_top} completed successfully")
 
-    # Display pertinent information (changes frequently)
-    print("\n**************************")       
-    for item in items_dictionary:
-        print(f"{item}: {items_dictionary[item]}")
-    print("**************************\n")
     
     # Take information and post it to our google sheet
     # Is not made available to general public. If wanted, create your own keys and give same file name.
@@ -100,6 +99,7 @@ if __name__ == "__main__":
     # Index tracking for updating fees rows 
     start_index = 1
     fee_index = 0
+    print('Updating worksheet')
     for item in items_dictionary:
         start_index += 1
         start_point_a = ('A', start_index)
