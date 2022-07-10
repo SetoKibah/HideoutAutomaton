09c4b733e925c2543358a0223e1aeb6c6928b4ef
NobylPistachio <KingChris396@gmail.com>
2022-07-10 00:28:23 -0400

    finished the script, took me long enough

--- testing.py
+++ testing.py
@@ -14,12 +14,20 @@ def itemVScomponentQuery(item_input):
     {{
         items(name: "{item_input}") {{
             name
-            sellFor{{
-                price
-                source
-            }}
             craftsFor{{
                 requiredItems{{
+                    item{{
+                        name
+                        sellFor{{
+                            price
+                            vendor{{
+                                name
+                            }}
+                        }}
+                    }}
+                    quantity
+                }}
+                rewardItems{{
                     item{{
                         name
                         sellFor{{
@@ -27,7 +35,7 @@ def itemVScomponentQuery(item_input):
                             source
                         }}
                     }}
-                quantity
+                    quantity
                 }}
             }}
         }}
@@ -36,51 +44,69 @@ def itemVScomponentQuery(item_input):
     """
     #cuts info down to dictionary that we use
     result = run_query(new_query)
-    trimmed_result = result['data']
-    trimmed_result = trimmed_result['items']
-    trimmed_result = trimmed_result[0]
+    trimmed_result = result['data']['items'][0]
+
 
     return trimmed_result
 
-def compare_itemprice_componentstotalprice(item_input):
-    #gives us
-    trimmed_result = itemVScomponentQuery(item_input)
-    item_seller_list = trimmed_result["sellFor"]
-    
+def highest_seller(list_of_vendors):
+    #must be the list of sellFor
     highest_price = 0
-    for seller in item_seller_list:
-        if seller["price"] > highest_price:
+    for seller in list_of_vendors:
+        if seller["price"] > highest_price and seller["source"] != "fleaMarket":
             highest_source = seller["source"]
             highest_price = seller["price"]
     item_highest_source = highest_source
     item_highest_price = highest_price
     
-    component_list = trimmed_result["craftsFor"]
-    component_list = component_list[0]
-    component_list = component_list["requiredItems"]
+    return (item_highest_price,item_highest_source)
 
+def highest_vendor(list_of_vendors):
+    #must be the list of sellFor
+    highest_price = 0
+    for seller in list_of_vendors:
+        if seller["price"] > highest_price and seller["vendor"]["name"] != "Flea Market":
+            highest_source = seller["vendor"]["name"]
+            highest_price = seller["price"]
+    item_highest_source = highest_source
+    item_highest_price = highest_price
+
+    return (item_highest_price,item_highest_source)
+
+def compare_itemprice_componentstotalprice(item_input):
+    #trimmed_information
+    trimmed_result = itemVScomponentQuery(item_input)["craftsFor"][0]
+    trimmed_reward = trimmed_result["rewardItems"][0]
+    trimmed_reward_seller_list = trimmed_reward["item"]["sellFor"]
+
+    #item information
+    reward_name = trimmed_reward["item"]["name"]
+    reward_quantity = trimmed_reward["quantity"]
+    reward_price = highest_seller(trimmed_reward_seller_list)[0]
+    reward_vendor = highest_seller(trimmed_reward_seller_list)[1]
+    reward_total_price = reward_quantity * reward_price
+    
+        #Note to self: maybe add a component_total_list= []
+    
+    #component information
+    trimmed_components = trimmed_result["requiredItems"]
     component_total_list= []
     component_total_price = 0
-    for component in component_list:
+    for component in trimmed_components:
         component_quantity = component["quantity"]
-        component_item = component["item"]
-        component_name = component_item["name"]
+        component_name = component["item"]["name"]
         component_total_list.append(f'{component_quantity}x {component_name}')
+        component_seller_list = component["item"]["sellFor"]
+        component_price = highest_vendor(component_seller_list)[0]
+        component_vendor = highest_vendor(component_seller_list)[1]
+        
+        component_total_price += component_quantity * component_price
 
-        component_seller_list = component_item["sellFor"]
-        for seller in component_seller_list:
-            if seller["price"] > highest_price:
-                highest_source = seller["source"]
-                highest_price = seller["price"]
-        component_highest_source = highest_source
-        component_highest_price = highest_price
-
-        component_total_price += component_quantity * component_highest_price
-    
-    print(f'{item_input}-{item_highest_price},{component_total_list}-{component_total_price}')
-    if item_highest_price > component_total_price:
+    #presenting and decision
+    print(f'{item_input}-{reward_total_price},{component_total_list}-{component_total_price}')
+    if reward_total_price > component_total_price:
         print("sell the item, you get more money")
-    elif item_highest_price == component_total_price:
+    elif reward_total_price == component_total_price:
         print("they are the same")
     else:
         print("sell it in components, you get more money")
