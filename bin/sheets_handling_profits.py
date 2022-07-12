f9477b5603e2b2f80fb63fa34283d21f0af15b29
SetoKibah <setokibah@gmail.com>
2022-07-12 09:17:20 -0600

    minor changes

--- bin/sheets_handling_profits.py
+++ bin/sheets_handling_profits.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.DEBUG, filename="Automaton.log", filemode="w",
                     format="%(asctime)s 0 %(levelname)s - %(message)s")
 
 # authenticating access
-sa = gspread.service_account(filename=r'C:\Users\Bradley\Desktop\HideoutAutomaton\pysheetskeys.json')
+sa = gspread.service_account(filename='pysheetskeys.json')
 # Connect to our spreadsheets
 sh = sa.open("Tarkov Butler")
 # Connect to our specific sheet
