493c3f7cdf34a44954dabf69c9465fdd2aa54ca6
SetoKibah <setokibah@gmail.com>
2023-01-20 12:07:47 -0700

    Added query logic to get more information, will look into adding more info grabbing and processing

--- bin/extract_ids.py
+++ bin/extract_ids.py
@@ -1,6 +1,7 @@
 import requests
 from time import sleep
-import csv
+import xlsxwriter
+import itertools
 
 def run_query(query):
     response = requests.post('https://api.tarkov.dev/graphql', json={'query': query})
@@ -19,29 +20,61 @@ def id_acquisition():
             id
             level
             station{{
-            name
+                name
+            }}
+            requiredItems{{
+                item{{
+                    name
+                    id
+                    avg24hPrice
+                }}
+                count
+            }}
+            rewardItems{{
+                count
+                item{{
+                    id
+                    name
+                    avg24hPrice
+                }}
+            
             }}
         }}
         }}
     }}
     """
+    # List creation
     id_list = []
-    id_list.append(['ID', 'Station', 'Station Level'])
+    id_list.append(['ID', 'Name', 'Station', 'Station Level'])
     result = run_query(new_query)
+    
     full_list = result['data']['hideoutStations']
+    components_list = []
+    
     for item in full_list:
         if item['crafts'] != []:
-            print(item['name'])
             for craft_item in item['crafts']:
-                id_list.append([craft_item['id'], craft_item['station']['name'], craft_item['level']])
-    return id_list
+                id_list.append([craft_item['rewardItems'][0]['item']['id'],craft_item['rewardItems'][0]['item']['name'], craft_item['station']['name'], craft_item['level']])
+                if craft_item['requiredItems'] != []:
+                    components_list.append([craft_item['requiredItems'][0]['item']['id'],craft_item['requiredItems'][0]['item']['name']])
+                    
+    return id_list, components_list
+
+
 if __name__ == "__main__":
-    ids = id_acquisition()
+    ids, components = id_acquisition()
+    components_sorted = list(set(map(tuple,components)))
+    print(len(components))
+    print(len(components_sorted))
+    components_sorted.insert(0, ['ID','Name'])
     
-    print(len(ids))
-    f = open('id_list.csv', 'w')
-    writer = csv.writer(f)
-    for id in ids:
-        print(id)
-        writer.writerow(id)
-    f.close()
+    with xlsxwriter.Workbook('ID and Names.xlsx') as workbook:
+        worksheet_one = workbook.add_worksheet('Craft Rewards')
+        worksheet_two = workbook.add_worksheet('Craft Components')
+        
+        for row_num, data in enumerate(ids):
+            worksheet_one.write_row(row_num, 0, data)
+        for row_num, data in enumerate(components_sorted):
+            worksheet_two.write_row(row_num, 0, data)
+            
+    
