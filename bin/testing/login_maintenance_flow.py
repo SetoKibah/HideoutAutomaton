cd64064b0ae3d1dacbd35caaeaabb709252cf63a
SetoKibah <setokibah@gmail.com>
2023-01-27 12:27:02 -0700

    updated to include 3 stations and one craft refresh

--- bin/testing/login_maintenance_flow.py
+++ bin/testing/login_maintenance_flow.py
@@ -8,21 +8,37 @@ from time import sleep
 
 # Collects and restarts the workbench craft
 def refresh_workbench():
-    auto.click(auto.moveTo(774,114, duration=.5))
+    while True:
+        if auto.locateOnScreen('bin\Workbench.PNG', confidence=.9):
+            auto.click(auto.moveTo(auto.locateCenterOnScreen('bin\Workbench.PNG', confidence=.9)))
+            break
+        else:
+            for i in range(0, 15):
+                auto.scroll(-1)
+        sleep(1)
+    auto.click(auto.moveTo(1588,824, duration=.5))
     sleep(2)
-    auto.click(auto.moveTo(1586,823, duration=.5))
-    sleep(2)
-    auto.click(auto.moveTo(1589,861, duration=.5))
-    sleep(2)
-    auto.click(auto.moveTo(959,785, duration=.5))
-    sleep(2)
-    auto.press('esc')
-
+    auto.click(auto.moveTo(1588,863, duration=.5))
+    auto.click(auto.moveTo(960,782, duration=1))
+    
 def refresh_lavatory():
-    pass
+    auto.click(auto.moveTo(auto.locateCenterOnScreen('bin\Lavatory.PNG', confidence=.9)))
+    sleep(2)
+    auto.click(auto.moveTo(1582,664, duration=.5))
+    sleep(2)
+    auto.click()
+    sleep(1)
+    auto.click(auto.moveTo(961,784, duration=.5))
+    auto.moveTo(932, 1002, duration=.5)
+    
 
 def refresh_medical():
-    pass
+    auto.click(auto.moveTo(1619,609, duration=.5))
+    sleep(2)
+    auto.click()
+    sleep(1)
+    auto.click(auto.moveTo(961,784, duration=.5))
+    auto.moveTo(932, 1002, duration=.5)
 
 # Logs into the game and awaits at menu
 def login():
@@ -46,6 +62,9 @@ def logout():
     auto.press('esc')
     sleep(1)
     auto.press('esc')
+    sleep(1)
+    auto.press('esc')
+    sleep(1)
     auto.click(auto.moveTo(956,951, duration=.5))
     sleep(.5)
     auto.press('y')
@@ -60,7 +79,7 @@ if __name__ == "__main__":
     interval = 1935
     runs = 10
     sleep(5)
-    
+    """
     while True:
         
         if auto.locateOnScreen('bin\Workbench.PNG', confidence=.9):
@@ -70,17 +89,16 @@ if __name__ == "__main__":
             print('See nothing...')
             auto.scroll(-1)
         sleep(1)
-    
-    
-    #login()
-    #refresh_workbench()
-    
-    
-    #for i in range(0, runs):
-    #    login()
-    #    refresh_workbench()
-    #    sleep(2)
-    #    logout()
-    #    print(f"Run {i+1} complete. Sleeping {interval/60} minutes...")
-    #    sleep(interval)
-    
+    """
+
+    login()
+    auto.click(auto.moveTo(1372, 26, duration=.05))
+    sleep(2)
+    refresh_medical()
+    sleep(2)
+    refresh_lavatory()
+    sleep(2)
+    refresh_workbench()
+    sleep(2)
+    #logout()
+    #sleep(interval)
