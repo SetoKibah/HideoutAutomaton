63594c312f6365af110dc39630a284a4d2ef686c
SetoKibah <setokibah@gmail.com>
2023-03-01 14:14:54 -0700

    Added arbitrage tool for future use

---
+++ bin/tools/arbitrage.py
@@ -0,0 +1,69 @@
+# This tool will find items within instant-profit range for reselling to traders
+# Requires trader price and average 24 hour price
+
+import requests
+from time import sleep, strftime
+import tools_sheets
+import sys
+
+
+def run_query(query):
+    response = requests.post('https://api.tarkov.dev/graphql', json={'query': query})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
+
+
+  
+def data_clean():
+    start_index = 2
+    query = f"""
+  {{
+      items{{
+          name
+          avg24hPrice
+          low24hPrice
+          sellFor{{
+              vendor{{
+                  name
+              }}
+              priceRUB
+          }}
+      }}
+  }}
+  """
+    data_raw = run_query(query)
+    data_list = data_raw['data']['items']
+    
+    for item in data_list:
+        name, average_price, low_price = item['name'], item['avg24hPrice'], item['low24hPrice']
+        
+        highest_vendor, highest_price = None, 0
+        for vendor in item['sellFor']:
+            print(vendor['vendor']['name'])
+            print(vendor['priceRUB'])
+            if int(vendor['priceRUB']) > highest_price:
+                if vendor['vendor']['name'] == "Flea Market":
+                    flea_price = int(vendor['priceRUB'])
+                else:
+                    highest_vendor = vendor['vendor']['name']
+                    highest_price = int(vendor['priceRUB'])
+        #abs_difference = abs(average_price - highest_price)
+        #print(f"Name: {name}\nAverage Flea: {average_price}\n{highest_vendor}: {highest_price}\nAbs Dif: {abs_difference}")
+        #print(name)
+        if low_price != None:
+            if highest_price > low_price:
+                print(f'Name: {name}')
+                print(f'Low 24hr vs highest price: {low_price} vs {highest_price}')
+                tools_sheets.update_single_cell(f'A{start_index}', name)
+                tools_sheets.update_single_cell(f'B{start_index}', average_price)
+                tools_sheets.update_single_cell(f'C{start_index}', low_price)
+                tools_sheets.update_single_cell(f'D{start_index}', highest_price)
+                tools_sheets.update_single_cell(f'E{start_index}', highest_vendor)
+                start_index += 1
+                sleep(5)
+                
+if __name__ == "__main__":
+    data_clean()
+    print('Complete')
---
+++ bin/tools/tools_sheets.py
@@ -0,0 +1,33 @@
+# Handles uploading data to google sheets for our database.
+
+# imports
+import gspread
+import datetime as dt
+import logging
+
+# Setup basic logging configuration
+logging.basicConfig(level=logging.DEBUG, filename="Automaton.log", filemode="w",
+                    format="%(asctime)s 0 %(levelname)s - %(message)s")
+
+# authenticating access
+sa = gspread.service_account(filename=r'C:\Users\arche\Desktop\HideoutAutomaton\bin\pysheetskeys.json')
+# Connect to our spreadsheets
+sh = sa.open("Tarkov Butler")
+# Connect to our specific sheet
+wks = sh.worksheet("Item Collection")
+logging.info('Google sheets authentication successful')
+
+
+# Function will search a specified column to get the bottom of the list.
+def get_next_empty_cell(column):
+    for i in range(1,999):
+        cell = wks.acell(f'{column}{i}').value
+        if cell == None:
+            start_point = (column, i)
+            break
+    logging.info(f"start looking for next empty cell {start_point}")
+    return start_point
+        
+# Used for updating one cell at a time, applicable for updating a list over time.
+def update_single_cell(cell, input_value):
+    wks.update(cell, input_value)
