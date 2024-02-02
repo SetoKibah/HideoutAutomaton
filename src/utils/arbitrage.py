76a6ac251b5301031c90ce1a49b3bb45cbe8fc5d
SetoKibah <setokibah@gmail.com>
2024-02-01 22:20:31 -0700

    minor changes to arbitrage

--- src/utils/arbitrage.py
+++ src/utils/arbitrage.py
@@ -1,12 +1,5 @@
-# This tool will find items within instant-profit range for reselling to traders
-# Requires trader price and average 24 hour price
-import collections
-import collections.abc
-collections.MutableMapping = collections.abc.MutableMapping
-import sys
 import requests
-from collections.abc import MutableMapping
-from time import sleep, strftime
+from time import sleep
 import tools_sheets
 
 
@@ -50,29 +43,24 @@ def data_clean():
         
         highest_vendor, highest_price = None, 0
         for vendor in item['sellFor']:
-            #print(vendor['vendor']['name'])
-            #print(vendor['priceRUB'])
             if int(vendor['priceRUB']) > highest_price:
                 if vendor['vendor']['name'] == "Flea Market":
                     flea_price = int(vendor['priceRUB'])
                 else:
                     highest_vendor = vendor['vendor']['name']
                     highest_price = int(vendor['priceRUB'])
-        #abs_difference = abs(average_price - highest_price)
-        #print(f"Name: {name}\nAverage Flea: {average_price}\n{highest_vendor}: {highest_price}\nAbs Dif: {abs_difference}")
-        #print(name)
-        if low_price != None:
-            if highest_price > low_price:
-             #   print(f'Name: {name}')
-             #   print(f'Low 24hr vs highest price: {low_price} vs {highest_price}')
-                tools_sheets.update_single_cell(f'A{start_index}', name)
-                tools_sheets.update_single_cell(f'B{start_index}', average_price)
-                tools_sheets.update_single_cell(f'C{start_index}', low_price)
-                tools_sheets.update_single_cell(f'D{start_index}', highest_price)
-                tools_sheets.update_single_cell(f'E{start_index}', highest_vendor)
-                start_index += 1
-                sleep(5)
+        
+        if low_price is not None and highest_price > low_price:
+            tools_sheets.update_single_cell(f'A{start_index}', name)
+            tools_sheets.update_single_cell(f'B{start_index}', average_price)
+            tools_sheets.update_single_cell(f'C{start_index}', low_price)
+            tools_sheets.update_single_cell(f'D{start_index}', highest_price)
+            tools_sheets.update_single_cell(f'E{start_index}', highest_vendor)
+            start_index += 1
+            sleep(5)
+        
         progress_bar(index + 1, len(data_list), start_index - 1)
+
 if __name__ == "__main__":
     data_clean()
     print('Complete')
